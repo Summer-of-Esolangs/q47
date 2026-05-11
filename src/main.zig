102b0e52fca07e59a9dac8c33ac5663e9ad38624
BradenEverson <bradeneverson@gmail.com>
2026-05-11 04:36:56 -0500

    Preprocess

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,39 @@
 const std = @import("std");
 const Io = std.Io;
 
+const preprocess = @import("preprocess.zig");
+const Op = preprocess.Op;
+
 pub fn main(init: std.process.Init) !void {
-    _ = init;
+    const io = init.io;
+    const alloc = init.gpa;
+
+    var args = init.minimal.args.iterate();
+    _ = args.next();
+
+    var source: []u8 = undefined;
+
+    if (args.next()) |file_path| {
+        source = try std.Io.Dir.cwd().readFileAlloc(
+            io,
+            file_path,
+            alloc,
+            .unlimited,
+        );
+    } else {
+        std.debug.print("Missing Q47 file!!!\n", .{});
+        std.process.exit(1);
+    }
+
+    defer alloc.free(source);
+
+    var top_level: Op = .{ .grouping = .empty };
+    defer top_level.deinit(alloc);
+
+    var data: []const u8 = source;
+
+    while (data.len > 0) {
+        const op, data = try preprocess.statement(alloc, data);
+        try top_level.grouping.append(alloc, op);
+    }
 }
--- src/preprocess.zig
+++ src/preprocess.zig
@@ -1,3 +1,80 @@
 //! Operation types
 
-pub const Op = union(enum) {};
+const std = @import("std");
+
+pub const ProcessError = error{
+    InvalidChar,
+    TODO,
+};
+
+pub const Condition = enum(u8) {
+    gt = '>',
+    lt = '<',
+    eq = '=',
+};
+
+pub const Op = union(enum) {
+    push: u8,
+    add,
+    mult,
+    neg,
+    reciprocal,
+    print_num,
+    print_char,
+    input,
+    conditional: struct { cond: Condition, comp: u8, action: std.ArrayList(Op) },
+    repeat: struct { times: u8, action: std.ArrayList(Op) },
+    jump_statement,
+    grouping: std.ArrayList(Op),
+    dup_top,
+    dup_second_top,
+    pop,
+    swap,
+    exp,
+    eof,
+
+    pub fn deinit(self: *Op, alloc: std.mem.Allocator) void {
+        switch (self.*) {
+            .grouping => |*g| {
+                for (g.items) |*s| s.deinit(alloc);
+                g.deinit(alloc);
+            },
+            .conditional => |*c| {
+                for (c.action.items) |*s| s.deinit(alloc);
+                c.action.deinit(alloc);
+            },
+            .repeat => |*r| {
+                for (r.action.items) |*s| s.deinit(alloc);
+                r.action.deinit(alloc);
+            },
+            else => {},
+        }
+    }
+};
+
+pub fn statement(alloc: std.mem.Allocator, data: []const u8) !struct { Op, []const u8 } {
+    if (data.len == 0) return .{ .eof, data };
+
+    switch (data[0]) {
+        '0'...'9' => |n| return .{ .{ .push = n - '0' }, data[1..] },
+        '+' => return .{ .add, data[1..] },
+        '*' => return .{ .mult, data[1..] },
+        '`' => return .{ .neg, data[1..] },
+        '/' => return .{ .reciprocal, data[1..] },
+        'p' => return .{ .print_num, data[1..] },
+        '@' => return .{ .print_char, data[1..] },
+        'i' => return .{ .input, data[1..] },
+        ';' => return .{ .jump_statement, data[1..] },
+        '.' => return .{ .dup_top, data[1..] },
+        ',' => return .{ .dup_second_top, data[1..] },
+        '?' => return .{ .pop, data[1..] },
+        ':' => return .{ .swap, data[1..] },
+        '^' => return .{ .exp, data[1..] },
+
+        ' ', '\t', '\n' => return statement(alloc, data[1..]),
+
+        '(' => return error.TODO, // TODO: Grouping
+        '{' => return error.TODO, // TODO: conditional or repeat
+        else => return error.InvalidChar,
+    }
+}
